31bbca55ee192e45d26920cee1decabc0552c21d
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-01-25 11:49:47 -0300

    add getQuizz by id  to services module

--- script/services.js
+++ script/services.js
@@ -6,4 +6,7 @@ export const services = {
     getQuizzes: async function getQuizzes() {
         return await axiosBase.get("/").then(resp => resp.data)
     },
+    getQuizz: async function getQuizz(id) {
+        return await axiosBase.get(`/${id}`).then(resp => resp.data)
+    },
 }
