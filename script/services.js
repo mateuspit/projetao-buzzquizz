e52164e6cf40f54fd13561c640a466a8e4db4ae2
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-01-25 11:46:20 -0300

    add getQuizzes to services module

--- script/services.js
+++ script/services.js
@@ -2,3 +2,8 @@ const axiosBase = axios.create({
     baseURL: 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes'
 });
 
+export const services = {
+    getQuizzes: async function getQuizzes() {
+        return await axiosBase.get("/").then(resp => resp.data)
+    },
+}
