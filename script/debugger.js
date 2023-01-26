aa50443037e277204155fdde23cfc45bb1d220be
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-01-26 10:48:37 -0300

    fix get element

--- script/debugger.js
+++ script/debugger.js
@@ -18,26 +18,12 @@ userQuizzes = quizzes.filter(quiz => quiz.id === userId); // COLETANDO QUIZZES D
 
 
 
-if(userQuizzes.length > 0){ //ANALISANDO SE O USUARIO POSSUI QUIZZES
+if (userQuizzes.length > 0) { //ANALISANDO SE O USUARIO POSSUI QUIZZES
     //PLOTANDO CABEÇALHO QUANDO O USER TEM QUIZZES ABAIXO
-    elementYourQuizzesSpace = document.querySelector(".yourQuizzesSpace");    
-    elementYourQuizzesSpace.innerHTML =  "";
+    elementYourQuizzesSpace = document.querySelector(".yourQuizzesMenu");
+    elementYourQuizzesSpace.innerHTML = "";
 
-    elementYourQuizzesSpace.innerHTML += `
-                <div class="yourQuizzes">
-                    <div class="yourQuizzesHeader">
-                        <div class="yourQuizzesTitle">
-                            Seus Quizzes
-                        </div>
-                        <div class="yourQuizzesHeaderButton">
-                            +                            
-                        </div>
-                    </div>                    
-                </div>
-                <div class="yourQuizzesMenu">    
-    `;
-
-    for(let i = 0; i < userQuizzes.length; i++){
+    for (let i = 0; i < userQuizzes.length; i++) {
         elementYourQuizzesSpace.innerHTML += `
         <div class="yourQuizz">
             <div class="yourQuizzImg">
@@ -49,10 +35,8 @@ if(userQuizzes.length > 0){ //ANALISANDO SE O USUARIO POSSUI QUIZZES
         </div>        
         `;
     }
-    elementYourQuizzesSpace.innerHTML += `</div>`   
-    console.log(elementYourQuizzesSpace);
 }
-else{
+else {
 
 }
 
