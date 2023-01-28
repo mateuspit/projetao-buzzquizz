771dc5f16bbe523cbb24a6dd15ccc3555e3b4baa
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-01-28 12:02:58 -0300

    add createQuizzContainerTemplate for screen-2 rendering

--- script/view.js
+++ script/view.js
@@ -1,8 +1,12 @@
-
-
 export const view = {
     createQuizzMainTitleTemplate: function createQuizzMainTitleTemplate(title, image) {
         return `<p style="background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${image})">${title}</p>`;
     },
-
+    createQuizzContainerTemplate: function createQuizzContainerTemplate(id) {
+        return `<div>
+                    <div id="${id}" class="quizz-container">
+                        ###!###
+                    </div>
+                </div>`;
+    }
 }
