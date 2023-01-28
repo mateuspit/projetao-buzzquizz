472c14dec68fdf6d925f74be344866f0fea56a90
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-01-28 12:04:47 -0300

    add createAnswerContainers for screen-2 rendering

--- script/view.js
+++ script/view.js
@@ -14,4 +14,11 @@ export const view = {
                     <p>${title}</p>
                 </div>`;
     },
+    createAnswersContainers: function createAnswersContainers() {
+        return `<div class="answer-container">
+                        <div>
+                        ###!###
+                        </div>
+                </div>`;
+    },
 }
